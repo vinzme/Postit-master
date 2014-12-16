7ff26fd86654fc07a3721ecbdd893fa6ae1c75f7
vinzme <vinzblue@gmail.com>
2014-12-17 00:45:32 +0800

    postit app

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ActiveRecord::Base
+  include Voteable
+  include Sluggable
+
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+
+  validates :title, presence: true, length: {minimum: 5}
+  validates :description, presence: true
+  validates :url, presence: true, uniqueness: true
+
+  sluggable_column :title
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  include Sluggable
+
+  has_many :posts
+  has_many :comments
+  has_many :votes
+
+  has_secure_password validations: false
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: {minimum: 5}
+
+  sluggable_column :username
+
+  def admin?
+    self.role == 'admin'
+  end
+
+  def moderator?
+    self.role == 'moderator?'
+  end
+
+end
